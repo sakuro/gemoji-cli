8a032c30dc998877004ab7b1e3c94f16e8d5ea92
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-02-24 01:12:16 +0900

    $ bundle gem gemoji-cli

---
+++ spec/gemoji/cli_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Gemoji::Cli do
+  it "has a version number" do
+    expect(Gemoji::Cli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
